c047d092cfc5ba8793f3b3e041f60c61b0c1f27e
Thomas Pani <thomas.pani@gmail.com>
2014-09-10 18:05:56 +0200

    Get karma running with phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,55 @@
 // Karma configuration
 
+module.exports = function(config) {
+  config.set({
 // base path, that will be used to resolve files and exclude
-basePath = '';
+basePath: '',
+
+frameworks: ['jasmine'],
 
 // list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
+files: [
+  'app/bower_components/jquery/jquery.js',
   'app/bower_components/angular/angular.js',
+  'app/bower_components/angular-resource/angular-resource.js',
+  'app/bower_components/angular-route/angular-route.js',
+  'app/bower_components/angular-sanitize/angular-sanitize.js',
   'app/bower_components/angular-mocks/angular-mocks.js',
+  'app/bower_components/angular-strap/dist/angular-strap.js',
+  'app/bower_components/spin.js/spin.js',
+  'app/bower_components/angular-spinner/angular-spinner.js',
   'app/scripts/*.coffee',
   'app/scripts/**/*.coffee',
   'test/mock/**/*.coffee',
   'test/spec/**/*.coffee'
-];
+],
+
+preprocessors: {
+  '**/*.coffee': 'coffee'
+},
 
 // list of files to exclude
-exclude = [];
+exclude: [],
 
 // test results reporter to use
 // possible values: dots || progress || growl
-reporters = ['progress'];
+reporters: ['progress'],
 
 // web server port
-port = 8080;
+port: 8080,
 
 // cli runner port
-runnerPort = 9100;
+runnerPort: 9100,
 
 // enable / disable colors in the output (reporters and logs)
-colors = true;
+colors: true,
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+logLevel: config.LOG_INFO,
 
 // enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+autoWatch: false,
 
 // Start these browsers, currently available:
 // - Chrome
@@ -46,11 +59,13 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers: ['PhantomJS'],
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+captureTimeout: 5000,
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-singleRun = false;
+singleRun: false
+});
+};
